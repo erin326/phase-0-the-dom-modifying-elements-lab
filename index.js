910ca960648f6418faf5b3f40adb39ecbf3c6b21
erin <erinverkoulen93@gmail.com>
2021-04-18 13:27:32 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "Erin is the champion";
+
+
+
+//const message = document.createElement('p');
+//message.textContent = 'ERIN is the champion';
+//newHeader.appendChild(message);
+
+
