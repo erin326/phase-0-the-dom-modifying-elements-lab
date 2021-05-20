e6b35f2fdb31afffe24ffae20a1a7a93352008ad
erin <erinverkoulen93@gmail.com>
2021-05-20 11:57:16 -0500

    commit tree dd59e0abb92926c4249063df84c338fda5cd88ab

--- index.js
+++ index.js
@@ -1,4 +1,76 @@
 // Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+const p = document.createElement('p');
+p.textContent = 'ERIN is the champion';
+newHeader.appendChild(p);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const element = document.createElement('div');
+
+document.body.appendChild(element);
+
+const ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+
+element.appendChild(ul); 
+
+element.style.backgroundColor = '#27647B';
+
+element.style.fontSize = '24px';
+element.style.marginLeft = '30px'
+;
+element.style.lineHeight = 2;
+
+element.classList.add('pet-listing', 'dog');
+
+const ul2 = document.getElementsByTagName('ul')[0];
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
+
+ul.remove();
+
+
+
+
+
+
+
+
 main.remove();
 
 const newHeader = document.createElement('h1');
@@ -6,7 +78,7 @@ newHeader.id = "victory";
 document.body.appendChild(newHeader);
 newHeader.innerHTML = "Erin is the champion";
 
-
+*/
 
 //const message = document.createElement('p');
 //message.textContent = 'ERIN is the champion';
